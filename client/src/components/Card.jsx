49a109df0f52d4b521171f2cc9d3d3966c9b921a
YashRaut24 <yashcodezzzz24@gmail.com>
2026-07-06 20:13:01 +0530

    Add draggable card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,3 +1,11 @@
-export function Card({ title }) {
-  return <div className="card">{title}</div>;
+export function Card({ id, title }) {
+  function handleDragStart(e) {
+    e.dataTransfer.setData("text/plain", id);
+  }
+
+  return (
+    <div className="card" draggable={true} onDragStart={handleDragStart}>
+      {title}
+    </div>
+  );
 }
