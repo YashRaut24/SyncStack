42d8a9904f88ed77add3d36e3a0976eb868f2282
YashRaut24 <yashcodezzzz24@gmail.com>
2026-07-07 13:59:54 +0530

    Add delete button to card

--- client/src/components/Card.jsx
+++ client/src/components/Card.jsx
@@ -1,11 +1,16 @@
-export function Card({ id, title }) {
+export function Card({ id, title, onDelete }) {
   function handleDragStart(e) {
     e.dataTransfer.setData("text/plain", id);
+  }
+
+  function handleDeleteClick() {
+    onDelete(id);
   }
 
   return (
     <div className="card" draggable={true} onDragStart={handleDragStart}>
       {title}
+      <button onClick={handleDeleteClick}>Delete</button>
     </div>
   );
 }
